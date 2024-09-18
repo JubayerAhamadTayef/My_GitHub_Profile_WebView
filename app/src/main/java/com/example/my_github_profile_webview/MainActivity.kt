6574012fffde4ff03f3cadc829292c7d95bb5532
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-18 22:39:21 +0600

    Write code for enable javaScript also use apply for concise code!

--- app/src/main/java/com/example/my_github_profile_webview/MainActivity.kt
+++ app/src/main/java/com/example/my_github_profile_webview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.my_github_profile_webview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.my_github_profile_webview.databinding.ActivityMainBinding
@@ -8,13 +9,19 @@ class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         supportActionBar?.hide()
 
-        binding.webView.loadUrl("https://github.com/JubayerAhamadTayef/")
+        binding.webView.apply {
 
+            loadUrl("https://github.com/JubayerAhamadTayef/")
+
+            settings.javaScriptEnabled = true
+
+        }
     }
 }
