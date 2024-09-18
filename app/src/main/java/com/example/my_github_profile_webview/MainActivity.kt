63cd7224a512cbf1b9f371364db5f8c587799b6f
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-18 22:24:17 +0600

    Start using binding!

--- app/src/main/java/com/example/my_github_profile_webview/MainActivity.kt
+++ app/src/main/java/com/example/my_github_profile_webview/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.my_github_profile_webview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.my_github_profile_webview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
